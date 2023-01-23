f956465fe713761e4fb2e387bc198f6b2af7779f
Sergei_L <lubeshkosergei@gmail.com>
2023-01-23 21:26:51 +0300

    feat:: solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let num_str = String(Math.abs(n))
+    let num_list=num_str.split('')
+    let reversedStr = num_list.reverse().join("");
+    return Number(reversedStr)
 }
+
+
+
+
